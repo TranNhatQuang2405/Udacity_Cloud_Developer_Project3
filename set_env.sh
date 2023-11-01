b48dec7ed6111807be0cee9117052b2d2c4cf3dc
Trần Nhất Quang <trannhatquang2405@gmail.com>
2023-11-01 09:43:59 +0700

    INIT

---
+++ set_env.sh
@@ -0,0 +1,10 @@
+# This file is used for convenience of local development.
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Abc12345!
+export POSTGRES_HOST=quangtn14.crro5wvz8xaa.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=quangtn14
+export AWS_BUCKET=arn:aws:s3:::quangtn14-project3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
